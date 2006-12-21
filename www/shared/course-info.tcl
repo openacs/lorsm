3d7e044bc82eeafedda5f0c170e9956f0bcc3507
Mario Aguado <maguado@innova.uned.es>
2006-12-21 08:24:12 +0000

    Resolve bug in indent

--- www/shared/course-info.tcl
+++ www/shared/course-info.tcl
@@ -88,6 +88,8 @@ append orgs_list "<tr class=\"list-header\">
 "
 db_foreach organizations {} {
 
+    set total_items [db_string items_count {select count(*) from ims_cp_items where org_id=:org_id} -default 0]
+
 
     append orgs_list "<tr class=\"list-even\"><td valign=\"top\" width=\"20%\">$org_title</td><td valign=\"top\" align=\"center\" width=\"5%\">$hasmetadata</td><td>"
 
@@ -99,15 +101,17 @@ db_foreach organizations {} {
     set track_id 0
     set table_extra_vars {return_url fs_local_package_id track_id}
     set table_def {
-	{ title "" "no_sort" "<td>$indent[if {![empty_string_p $identifierref]} {set href \"<a href='[lorsm::fix_href2 -item_id $item_id -identifierref $identifierref -fs_package_id $fs_package_id -fs_local_package_id $fs_local_package_id -folder_id $folder_id -type $type -track_id $track_id]' target='body' title='$item_title'>$item_title</a>\"} else {set href $item_title}]</td>" }
-	{ metadata "Metadata?" "no_sort" "<td align=\"center\">[if {$hasmetadata == \"f\"} {set hasmetadata \"No\"} else {set hasmetadata \"<a href=md/?ims_md_id=$item_id>Metadata\"}]</a></td>" }
-	{ type   "Type" "no_sort" "<td align=\"center\">$type</td>" }
+	{ title "\#lorsm.Item_Name\#" "no_sort" "<td>
+           [set indent  \"\"
+                      for { set i 0 } { $i < [expr $indent -1]} { incr i } {
+                      append indent \"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"
+                      }
+           if {![empty_string_p $identifierref]} {set href \"<a href='[lorsm::fix_href2 -item_id $item_id -identifierref $identifierref -fs_package_id $fs_package_id -fs_local_package_id $fs_local_package_id -folder_id $folder_id -type $type -track_id $track_id]' target='body' title='$item_title'>$item_title</a>\"} else {set href $item_title}]</td>"	}
+	{ metadata "\#lorsm.Metadata_1\#" "no_sort" "<td align=\"center\">[if {$hasmetadata == \"f\"} {set hasmetadata \"No\"} else {set hasmetadata \"<a href=md/?ims_md_id=$item_id>Metadata\"}]</a></td>" }
+	{ type   "\#lorsm.Type\#" "no_sort" "<td align=\"center\">$type</td>" }
     }
 
-    set table_item [ad_table -Tmissing_text $missing_text -Textra_vars $table_extra_vars  -Theader_row_extra "style=\"background-color: #e0e0e0; font-weight: bold;\" class=\"list-header\"" -Ttable_extra_html $table_extra_html blah {
-        
-
-    } $table_def]
+    set table_item [ad_table -Tmissing_text $missing_text -Textra_vars $table_extra_vars -Theader_row_extra "style=\"background-color: #e0e0e0; font-weight: bold;\" class=\"list-header\"" -Ttable_extra_html $table_extra_html blah { } $table_def]
 
     append orgs_list "$table_item"
 

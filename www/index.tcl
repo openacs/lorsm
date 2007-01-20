6646ab19b1d7d8c249c2451bd909b9dabcd02abd
Don Baccus <dhogaza@pacifier.com>
2007-01-20 02:09:59 +0000

    Added an index file for convenience, which redirects to the admin page.

---
+++ www/index.tcl
@@ -0,0 +1,10 @@
+ad_page_contract {
+    
+    Index page for lorsm.  Redirect to the admin pages.
+
+    @author Don Baccus (dhogaza@pacifier.com)
+    @creation-date 2007-19-14
+    @cvs-id $Id$
+}
+
+ad_returnredirect admin

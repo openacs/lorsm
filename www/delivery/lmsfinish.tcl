c716418e68796e8d98658fcfe9598db2715daf1c
Michele Slocovich <michele@sii.it>
2007-07-10 16:12:10 +0000

    Adding procedure called when user switching from one sco to the next without the sco itself calling LMSFinish so not to incur a double LMSInitialise nor to mix the data from one sco to the next over. (Basically the page will call LMSFinish on behalf of the SCO)

---
+++ www/delivery/lmsfinish.tcl
@@ -0,0 +1,27 @@
+# packages/lorsm/www/delivery-scorm/lmsfinish.tcl
+
+ad_page_contract {
+    
+    called if an user is swithing between items of the same manifest,
+	WITHOUT the content having previously called LMSFinish   
+
+    @author Michele Slocovich ()
+    @creation-date 2004-07-04
+} {
+    item_id:optional,integer,notnull
+    man_id:integer,notnull
+    initedonpage:integer,notnull
+} -properties {
+} -validate {
+} -errors {
+} 
+
+if { ! [info exists item_id] } {
+        set item_id 0
+}
+
+#this should automatically happen. nevertheless we put it here
+#ad_set_client_property lorsm studenttrack ""
+#ad_set_client_property lorsm currenttrackid ""
+#ad_set_client_property lorsm initedonpage ""
+ 

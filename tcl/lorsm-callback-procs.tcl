f0af0c7529abd9cb50ad210979132e58a9681b4b
Don Baccus <dhogaza@pacifier.com>
2009-02-15 01:46:46 +0000

    Changed stupid dotlrn_users reference to users

--- tcl/lorsm-callback-procs.tcl
+++ tcl/lorsm-callback-procs.tcl
@@ -32,14 +32,14 @@ ad_proc -callback application-track::getSpecificInfo -impl lorsm {} {
     upvar $elements_name my_elements
 
     set my_query {
-        select d.id,i.course_name,l.start_time,l.end_time
-        from lorsm_student_track l, dotlrn_users d, ims_cp_manifests i
+        select u.id,i.course_name,l.start_time,l.end_time
+        from lorsm_student_track l, users u, ims_cp_manifests i
         where l.course_id IN (
             select distinct l.course_id
             from lorsm_student_track l
             where l.community_id=:class_instance_id
                 group by l.course_id)
-            and l.user_id = d.user_id
+            and l.user_id = u.user_id
             and l.course_id = i.man_id
     }
     set my_elements ""
